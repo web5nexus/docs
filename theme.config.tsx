5da9d0f138db48b617b58304e70abac6d411c60a
Jetso Analin <jetyjetso@gmail.com>
2023-06-05 06:00:01 +0530

    New Configs Added

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig,useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
-  logo: <img width={50} src='https://i.ibb.co/yShkFGZ/w5BlueT.png'></img>,
+  logo: (
+    <>
+      <img width={50} src='https://i.ibb.co/yShkFGZ/w5BlueT.png'></img>
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+      Web5 Nexus
+      </span>
+    </>
+  ),
   project: {
     link: 'https://github.com/web5nexus',
   },
@@ -25,7 +33,25 @@ const config: DocsThemeConfig = {
     next: true
   },
   gitTimestamp:true,
-  
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://docs.web5.nexus' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Web5 Nexus'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Web3 Simplified: One SDK, Endless Possibilities for Seamless Integration & User Retention'}
+        />
+        <link rel="icon" type="image/png" href="https://i.ibb.co/yShkFGZ/w5BlueT.png"></link>
+      </>
+    )
+  }
 }
 
 export default config
